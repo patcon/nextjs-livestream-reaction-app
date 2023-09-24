478c169f00c54f35503d3bcb682738169e27360d
Patrick Connolly <patrick.c.connolly@gmail.com>
2023-09-24 05:50:49 +0100

    Updated to using prod api key.

--- liveblocks.config.js
+++ liveblocks.config.js
@@ -1,7 +1,7 @@
 import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
-const DEFAULT_LIVEBLOCKS_PUBLIC_API_KEY = "pk_dev_gzes2UM2EEtK7zVfozlZFp1TVyMtzSDXwtr8M_jzExAROw5kkFrrQ6rXSFgyC--J"
+const DEFAULT_LIVEBLOCKS_PUBLIC_API_KEY = "pk_prod_P_0wGbwj0rlW1SqT_U8NpvgZb4o8ZG2HkaaBCWaFwwYJ_bb8FErTqPBY9AOjl-wI"
 
 const client = createClient({
   publicApiKey: process.env.NEXT_PUBLIC_LIVEBLOCKS_PUBLIC_API_KEY ?? DEFAULT_LIVEBLOCKS_PUBLIC_API_KEY,
